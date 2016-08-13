22d0d0d5b21dee643a9dca7416eeb5c906466e93
Austin Kabiru <austin.kabiru@andela.com>
2016-08-13 08:28:06 +0000

    feat(ultrasoni): detect distance

--- litt.py
+++ litt.py
@@ -2,8 +2,13 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3,GPIO.OUT)     #Define pin 3 as an output pin
 
+LED_PIN = 3 #Define pin 3 as an output pin
+TRIG = 23 # Associate pin 23 to TRIG
+ECHO = 24 # Associate pin 24 to ECHO
+
+GPIO.setup(LED_PIN,GPIO.OUT) 
+   
 def check_distance(time):
     SPEED = 17.150
     return SPEED * time
@@ -13,9 +18,13 @@ print check_distance(30)
 def light_it_up():
     distance = check_distance(30)
     if distance > 100:
-        GPIO.output(3,1)
+        while True:
+            GPIO.output(LED_PIN,1)
+            time.sleep(1)
+            GPIO.output(LED_PIN, 0)
+            time.sleep(1)
     else:
-        GPIO.output(3,0) 
+        GPIO.output(LED_PIN,0) 
 
 
 light_it_up() 
